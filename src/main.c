fde6b1f8d47610f21bdeae987bc26d57d0a77f55
8dcc <8dcc.git@gmail.com>
2023-04-09 23:48:14 +0200

    Add doxygen comments to main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,28 @@
 
 #include <stdio.h>
 
+/**
+ * @brief   The PI number
+ */
+#define PI 3.141592
+
+/**
+ * Simple function for adding 2 integers.
+ *
+ * @brief   Sum a + b
+ * @todo    Rename function
+ * @param   a, b The numbers to sum
+ * @return  Sum of a + b
+ * @author  8dcc
+ */
+int test_func(int a, int b) {
+    return a + b;
+}
+
+/**
+ * @todo    Argument handling
+ * @brief   Entry point of the program
+ */
 int main(int argc, char** argv) {
     printf("Hello, world!\n");
     return 0;
