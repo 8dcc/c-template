83a4beced350d7b77e99b76f16440749673ceed9
8dcc <8dcc.git@gmail.com>
2023-04-10 15:01:53 +0200

    Compact doxygen comments

--- src/main.c
+++ src/main.c
@@ -10,11 +10,8 @@
  * Simple function for adding 2 integers.
  *
  * @todo Rename function
- *
  * @brief Sum a + b
- *
  * @param a, b The numbers to sum
- *
  * @return Sum of a + b
  */
 int test_func(int a, int b) {
@@ -23,12 +20,9 @@ int test_func(int a, int b) {
 
 /**
  * @todo Argument handling
- *
  * @brief Entry point of the program
- *
  * @param argc Number of arguments
  * @param argv Vector of string arguments
- *
  * @return Exit code
  */
 int main(int argc, char** argv) {
